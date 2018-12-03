3b1ca089ec41f41d8c40f664b03238014b2b3776
sanzmata1834 <sanzmata1834@he-ferrer.eu>
2018-12-03 04:58:12 +0100

    Modifs PHP inscription

--- inscription.php
+++ inscription.php
@@ -9,12 +9,5 @@ $mdp1 = $_POST["mdp1"];
 $mdp2 = $_POST["mdp2"];
 
 echo "Vous êtes bien enregistrés, voici vos données :<br>";
-echo "<p>Votre nom est : ".$nom."</p>";
-echo "<p>Votre prenom est : ".$prenom."</p>";
-echo $date;
-echo $cp;
-echo $ville;
-echo $mail;
-echo $mdp1;
-echo $mdp2;
+echo $nom, $prenom, $date, $cp, $ville, $mail, $mdp1, $mdp2;
 ?>
